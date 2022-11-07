e47230c5a0e59e925b827a1125ee885bee577895
T1me2 <jmiller10@css.edu>
2022-11-07 17:03:30 -0600

    hw for week 6

---
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "92fc4314e0fe612aadab28712e4a9d97"
+
+# Geoapify API Key
+geoapify_key = "AIzaSyA1tmJIzF4MzhnctQbr398iHEXKjs6M7vg"
+
+#Second Geoapify key
+second_key = '0a3d04a146b945db98da7bd810057a5f'
